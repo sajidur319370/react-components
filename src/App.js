56140c4d50731c0dbdb9d9a8d1aac3fde1758eab
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-03-20 18:44:17 +0600

    render added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Device from "./components/Device/Device";
+import Tablets from "./components/Tablets/Tablets";
 import Watch from "./components/Watch/Watch";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Device name="Aphone" price="120,000"></Device>
       <Watch></Watch>
+      <Tablets></Tablets>
     </div>
   );
 }
